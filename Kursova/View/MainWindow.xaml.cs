2d0fccae2f9c28b63f50902ee107de38e351ea3e
DmytroBabiak <dmytro.babiak.pz.2022@lpnu.ua>
2024-12-04 00:28:41 +0200

    Add project files.

---
+++ Kursova/View/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Kursova.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindow();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
