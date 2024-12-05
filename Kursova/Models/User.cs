a44346d5658c471e76a42721bbdc36bc4d1a604b
DmytroBabiak <dmytro.babiak.pz.2022@lpnu.ua>
2024-12-05 22:28:53 +0200

    Add UserProfile

--- Kursova/Models/User.cs
+++ Kursova/Models/User.cs
@@ -2,11 +2,11 @@
 {
     public class User
     {
-        public int UserId { get; set; }  // Ідентифікатор користувача
-        public string Username { get; set; }  // Логін
-        public string Password { get; set; }  // Пароль
-        public string Email { get; set; }  // Електронна пошта
-        public string FullName { get; set; }  // Повне ім'я
-        public DateTime DateOfBirth { get; set; }  // Дата народження
+        public int UserId { get; set; }  
+        public string Username { get; set; }  
+        public string Password { get; set; } 
+        public string Email { get; set; }  
+        public string FullName { get; set; } 
+        public DateTime DateOfBirth { get; set; }  
     }
 }
--- Kursova/ViewModels/UserProfileViewModel.cs
+++ Kursova/ViewModels/UserProfileViewModel.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Kursova.Models;
+using System.ComponentModel;
+using System.Windows.Input;
+
 
 namespace Kursova.ViewModels
 {
-    internal class UserProfileViewModel
+    public class UserProfileViewModel : INotifyPropertyChanged
     {
+        private User _currentUser;
+
+        public User CurrentUser
+        {
+            get => _currentUser;
+            set
+            {
+                _currentUser = value;
+                OnPropertyChanged(nameof(CurrentUser));
+            }
+        }
+
+        public ICommand SaveChangesCommand { get; }
+
+        public UserProfileViewModel()
+        {
+            // Ініціалізуйте профіль користувача
+            CurrentUser = new User
+            {
+                Username = "Ivan",
+                FullName= "Petrenko",
+                Email = "ivan.petrenko@example.com"
+            };
+
+            SaveChangesCommand = new RelayCommand<object>(
+            execute: _ => SaveChanges(),
+            canExecute: _ => true
+            );
+
+        }
+
+        private void SaveChanges()
+        {
+
+            System.Diagnostics.Debug.WriteLine($"User {CurrentUser.Username} {CurrentUser.FullName} saved.");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
